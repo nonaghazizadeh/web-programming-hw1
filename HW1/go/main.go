0d8c2ac90619e4de3d8ec68ab8289eb78d8416fe
rerfan24 <rezaerfan26@gmail.com>
2021-10-25 17:18:07 +0330

    add: GET method and fix: POST method

--- HW1/go/main.go
+++ HW1/go/main.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-redis/redis/v8"
 )
 
+type postJSON struct {
+	Data string
+}
+
 var ctx = context.Background()
 
-func redisHandler(action string, message string) string {
+func redisHandler(action string, message string, c *gin.Context) {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "0.0.0.0:7001",
 		Password: "", // no password set
@@ -20,67 +27,65 @@ func redisHandler(action string, message string) string {
 	})
 
 	switch action {
+
 	case "Set":
 		h := sha256.New()
 		h.Write([]byte(message))
 		encodedSHA := hex.EncodeToString(h.Sum(nil))
 		err := rdb.Set(ctx, encodedSHA, message, 0).Err()
 		if err != nil {
-			return "something went wrong with database"
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "something went wrong with database",
+			})
 		}
-		return encodedSHA
+		c.JSON(http.StatusOK, gin.H{
+			"message": encodedSHA,
+		})
+
 	case "Get":
+		data, err := rdb.Get(ctx, message).Result()
+		if err == redis.Nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "Data doesn't exist",
+			})
+		} else if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "something went wrong with database",
+			})
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"message": data,
+			})
+		}
 
 	default:
 	}
-	return ""
-	// err := rdb.Set(ctx, "key", "value", 0).Err()
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// val, err := rdb.Get(ctx, "key").Result()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println("key", val)
-
-	// val2, err := rdb.Get(ctx, "key2").Result()
-	// if err == redis.Nil {
-	// 	fmt.Println("key2 does not exist")
-	// } else if err != nil {
-	// 	panic(err)
-	// } else {
-	// 	fmt.Println("key2", val2)
-	// }
-	// Output: key value
-	// key2 does not exist
 }
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/go/sha256/:req", func(c *gin.Context) {
-
+	r.GET("/go/sha256/:data", func(c *gin.Context) {
+		data := c.Param("data")
+		redisHandler("Get", data, c)
 	})
 
-	r.POST("/go/sha256", func(c *gin.Context) {
-		data := c.Query("data")
-		if len([]rune(data)) < 8 {
+	r.POST("/go/sha256/:data", func(c *gin.Context) {
+		jsonData, err := ioutil.ReadAll(c.Request.Body)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": "something went wrong with database",
+			})
+		}
+		var data postJSON
+		json.Unmarshal(jsonData, &data)
+		fmt.Println(data.Data)
+		if len([]rune(data.Data)) < 8 {
 			c.JSON(http.StatusBadRequest, gin.H{
 				"message": "Your message lenght must be more than 8 characters!",
 			})
 		} else {
-			mess := redisHandler("Set", data)
-			if mess == "something went wrong with database" {
-				c.JSON(http.StatusInternalServerError, gin.H{
-					"message": mess,
-				})
-			} else {
-				c.JSON(http.StatusOK, gin.H{
-					"message": mess,
-				})
-			}
+			redisHandler("Set", data.Data, c)
 		}
 	})
 
